d476fd166c7fb36ba927726392b13786bcda1368
cayetanobv <cayetano.benavent@geographica.gs>
2015-06-19 10:26:54 +0200

    updated download link to Gdal2 new release

--- getsources_gdal2.sh
+++ getsources_gdal2.sh
@@ -3,7 +3,7 @@
 # Get GDAL2 sources from GitHub
 
 cd /tmp && mkdir gdal2 && cd gdal2
-wget http://download.osgeo.org/gdal/2.0.0/gdal-2.0.0RC1.tar.gz
-tar -xzvf gdal-2.0.0RC1.tar.gz
-rm gdal-2.0.0RC1.tar.gz
+wget http://download.osgeo.org/gdal/2.0.0/gdal-2.0.0.tar.gz
+tar -xzvf gdal-2.0.0.tar.gz
+rm gdal-2.0.0.tar.gz
 exit
