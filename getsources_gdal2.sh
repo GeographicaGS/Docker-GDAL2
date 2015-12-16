a874b3bc2edf3db9ed887433e378e4f2c1c73c4d
Alberto Asuero <alberto.asuero@geographica.gs>
2015-12-16 11:11:07 +0100

    Move to folder and improving doc

--- getsources_gdal2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env sh
-
-# Get GDAL2 sources from GitHub
-
-cd /tmp && mkdir gdal2 && cd gdal2
-wget http://download.osgeo.org/gdal/2.0.1/gdal-2.0.1.tar.gz
-tar -xzvf gdal-2.0.1.tar.gz
-rm gdal-2.0.1.tar.gz
-exit
